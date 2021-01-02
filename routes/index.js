73a0646101838d09b9a5d1cc75a7de192612ed6c
kanoyami <kongzenwang@tencent.com>
2021-01-02 23:47:04 +0800

    add stickers

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var path = require('path')
 var fs = require("fs")
 var config = require("../config/config.json")
 const FONTS_DIR = path.join(process.cwd(), "upload", "fonts")
+const STICKERS_DIR = path.join(process.cwd(), "upload", "stickers")
 /* GET home page. */
 //'w.Write([]byte(`{"version": " + BackendVersion + ", "config": {"enableTranslate":  + strconv.FormatBool(EnableTranslate) + }}))'
 router.get('/server_info', function (req, res, next) {
@@ -33,6 +34,39 @@ router.post('/font_upload', function (req, res, next) {
 
 });
 
+
+router.post('/sticker_upload', function (req, res, next) {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'no file uploaded' });
+  }
+
+  const file = req.files.file;
+  const file_name = file.name
+  const file_path = path.join(STICKERS_DIR, file_name)
+  file.mv(file_path, err => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(err);
+    }
+    res.json({ fileName: file.name, filePath: `/upload/stickers/${file_name}` });
+  });
+
+});
+
+
+router.get('/stickers_list', function (req, res, next) {
+  fs.readdir(STICKERS_DIR, (err, files) => {
+    if (!files) return res.json([])
+    let res_arr = []
+    files.forEach(e => {
+      res_arr.push({
+        url: `/upload/stickers/${e}`,
+        name: e
+      })
+    })
+    res.json(res_arr)
+  })
+});
 
 
 router.get('/fonts_list', function (req, res, next) {
